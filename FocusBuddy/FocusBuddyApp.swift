a73d2e8b9ca0e066701ddc6a72601226fd76c73d
Dave Rosales <davealvarrez93@gmail.com>
2025-07-11 19:05:09 -0600

    Initial Commit

---
+++ FocusBuddy/FocusBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBuddyApp.swift
+//  FocusBuddy
+//
+//  Created by Dave Rosales on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusBuddyTests/FocusBuddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  FocusBuddyTests.swift
+//  FocusBuddyTests
+//
+//  Created by Dave Rosales on 7/11/25.
+//
+
+import Testing
+
+struct FocusBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
